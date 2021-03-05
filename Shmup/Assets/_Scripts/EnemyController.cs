1d00cdd7dc06a6ff5efd9668aaa0dcbc782d10cd
unknown <thiagovcs@al.insper.edu.br>
2021-03-05 09:09:46 -0300

    shmupTutorial1

---
+++ Shmup/Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : SteerableBehaviour, IShooter, IDamageable
+{
+
+    public void Shoot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    float angle = 0;
+
+    private void FixedUpdate()
+    {
+        angle += 0.1f;
+        Mathf.Clamp(angle, 0.0f, 2.0f * Mathf.PI);
+        float x = Mathf.Sin(angle);
+        float y = Mathf.Cos(angle);
+
+        Thrust(x, y);
+       
+    }
+}
---
+++ Shmup/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : SteerableBehaviour, IShooter, IDamageable
+{
+    Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void Shoot()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void FixedUpdate()
+    {
+        float yInput = Input.GetAxis("Vertical");
+        float xInput = Input.GetAxis("Horizontal");
+        Thrust(xInput, yInput);
+        if (yInput != 0 || xInput != 0)
+        {
+            animator.SetFloat("Velocity", 1.0f);
+        }
+        else
+        {
+            animator.SetFloat("Velocity", 0.0f);
+        }
+    }    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Inimigos"))
+        {
+            Destroy(collision.gameObject);
+            TakeDamage();
+        }
+    }
+    
+}
